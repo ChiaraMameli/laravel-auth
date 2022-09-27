3f105b8e2e963b1dabbc64c5125ec1b98aa539a4
ChiaraMameli <mameli.chiara@libero.it>
2022-09-27 15:08:42 +0200

    refactoring

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
+
+Auth::routes(['register' => false]);
+
+Route::get('/admin', 'HomeController@index')->name('admin.home');
